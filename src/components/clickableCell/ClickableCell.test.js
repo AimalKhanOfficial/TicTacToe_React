af7a48aee96b0f3fdaaed9a4b57baee0a97cc775
Aimal Khan <aimalkhanofficial@gmail.com>
2019-09-28 13:48:25 -0400

    Failing test added for clicking a Clickable event!

--- src/components/clickableCell/ClickableCell.test.js
+++ src/components/clickableCell/ClickableCell.test.js
@@ -7,3 +7,10 @@ it('Clickable Cell should have some text', () => {
     let baseAreaWrapper = shallow(<ClickableCell cellId={1}/>);
     expect(baseAreaWrapper.text().length).toBeGreaterThan(0);
 })
+
+it('Clickable Cell onClick should respond to click event', () => {
+    const fnClick = jest.fn();
+    let baseAreaWrapper = shallow(<ClickableCell cellId={1} onBoxClick={fnClick}/>);
+    baseAreaWrapper.find('button').simulate('click');
+    expect(fnClick).toHaveBeenCalled();
+})

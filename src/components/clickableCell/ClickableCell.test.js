1a9787cbc526cd9b65ce0788faee80d049204b85
Aimal Khan <aimalkhanofficial@gmail.com>
2019-09-28 13:19:55 -0400

    Adding a simple shallow render test for Clickable Cell component!

--- src/components/clickableCell/ClickableCell.test.js
+++ src/components/clickableCell/ClickableCell.test.js
@@ -2,3 +2,8 @@ import React from 'react';
 import { shallow } from 'enzyme';
 import ClickableCell from './ClickableCell';
 import '../../../EnzymeSetup'
+
+it('Clickable Cell should have some text', () => {
+    let baseAreaWrapper = shallow(<ClickableCell/>);
+    expect(baseAreaWrapper.text().length).toBeGreaterThan(0);
+})

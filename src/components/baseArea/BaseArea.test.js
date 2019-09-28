7fd9e86da221c86e4647a5bc36c3999a7b87facf
Aimal Khan <aimalkhanofficial@gmail.com>
2019-09-28 14:02:26 -0400

    Failing Test: When the ClickableCell is clicked, the base area state should keep track of it.

--- src/components/baseArea/BaseArea.test.js
+++ src/components/baseArea/BaseArea.test.js
@@ -15,3 +15,9 @@ it('Base Area prop with undefined props should not fail', () => {
 it('Base Area prop having a nested array with undefined should not Throw Error', () => {
     expect(() => shallow(<BaseArea baseAreaCells={[ undefined ]}/>)).not.toThrowError();
 })
+
+it('Base Area State should maintain a record of which box was clicked', () => {
+    let baseAreaWrapper = shallow(<BaseArea baseAreaCells={[[1]]}/>);
+    baseAreaWrapper.instance().boxClicked(1);
+    expect(baseAreaWrapper.instance().state.filledBoxes.length).toBeGreaterThan(0);
+})

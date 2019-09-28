74c561a83851a324f05501c09f6da528c5e9c4af
Aimal Khan <aimalkhanofficial@gmail.com>
2019-09-28 13:17:11 -0400

    Adding a Clickable Cell component as part of the Refactor process. This will be used to create small clickable cells of the playing base area!

---
+++ src/components/clickableCell/ClickableCell.jsx
@@ -0,0 +1,11 @@
+import React, {Component} from "react";
+
+export default class ClickableCell extends Component {
+    render(){
+        return (
+            <div>
+
+            </div>
+        );
+    }
+}

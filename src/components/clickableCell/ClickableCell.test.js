9f81bdd3444252d7fddeb4a1b2c605c7a86e8ea0
Aimal Khan <aimalkhanofficial@gmail.com>
2019-09-28 13:22:16 -0400

    Clickable Component now accepts a cellId prop param. This would be used to determine which cell was clicked. (Test Case Passing.)

--- src/components/clickableCell/ClickableCell.test.js
+++ src/components/clickableCell/ClickableCell.test.js
@@ -4,6 +4,6 @@ import ClickableCell from './ClickableCell';
 import '../../../EnzymeSetup'
 
 it('Clickable Cell should have some text', () => {
-    let baseAreaWrapper = shallow(<ClickableCell/>);
+    let baseAreaWrapper = shallow(<ClickableCell cellId={1}/>);
     expect(baseAreaWrapper.text().length).toBeGreaterThan(0);
 })

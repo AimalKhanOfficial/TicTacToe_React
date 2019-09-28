e49e103c5f49bdfb57071cf5a31e09290f5281cb
Aimal Khan <aimalkhanofficial@gmail.com>
2019-09-28 11:48:49 -0400

    Base Area Component added to the App.js class. Props used to accept a text param!

--- src/components/baseArea/BaseArea.jsx
+++ src/components/baseArea/BaseArea.jsx
@@ -3,7 +3,9 @@ import React, {Component} from "react";
 export default class BaseArea extends Component {
     render() {
         return (
-            <div>Example Text!</div>
+            <div>
+                {this.props.baseAreaCells}
+            </div>
         );
     }
 }

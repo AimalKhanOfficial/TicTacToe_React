6fc5f7a7f37c8fd46bcc98d499a7241d295b0dfc
Aimal Khan <aimalkhanofficial@gmail.com>
2019-09-29 13:50:56 -0400

    Failing Tests: Passing Undefined playerMoves or winningPatterns to the checkresult method should return false.

--- src/Helpers/tests/Utilities.test.js
+++ src/Helpers/tests/Utilities.test.js
@@ -1,7 +1,7 @@
 import checkResult from '../Utilities';
 import Constants from '../Constants';
 
-it("passing valid winning patterns and player moves to checkResult method should return true", () => {
+it("passing valid winning patterns and playerMoves to checkResult method should return true", () => {
     expect(checkResult([
         {
             cellId: 11,
@@ -16,4 +16,25 @@ it("passing valid winning patterns and player moves to checkResult method should
             playerFlag: true
         }
     ], true, Constants.winningPatterns)).toBe(true);
+});
+
+it("passing undefined winning patterns to checkResult method should return false", () => {
+    expect(checkResult([
+        {
+            cellId: 11,
+            playerFlag: true
+        },
+        {
+            cellId: 12,
+            playerFlag: true
+        },
+        {
+            cellId: 13,
+            playerFlag: true
+        }
+    ], true, undefined)).toBe(false);
+});
+
+it("passing undefined playerMoves to checkResult method should return false", () => {
+    expect(checkResult(undefined, true, Constants.winningPatterns)).toBe(false);
 });

c69a9aedac6d90f215f3d3f57598614914b935a5
Aimal Khan <aimalkhanofficial@gmail.com>
2019-09-29 14:12:28 -0400

    Failing Test: When the isGameFinished Attr is marked true, the body should provide a valid Game Over message.

--- src/components/baseArea/BaseArea.test.js
+++ src/components/baseArea/BaseArea.test.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { shallow } from 'enzyme';
 import BaseArea from './BaseArea';
 import '../../../EnzymeSetup'
+import Constants from '../../Helpers/Constants';
 
 it('Base Area should have some text', () => {
     let baseAreaWrapper = shallow(<BaseArea baseAreaCells={[[1]]}/>);
@@ -16,7 +17,7 @@ it('Base Area prop having a nested array with undefined should not Throw Error',
     expect(() => shallow(<BaseArea baseAreaCells={[ undefined ]}/>)).not.toThrowError();
 })
 
-describe("Base Area wrapper with same Arrange phase", () => {
+describe('Base Area wrapper with same Arrange phase', () => {
     let baseAreaWrapper;
     beforeEach(() => baseAreaWrapper = shallow(<BaseArea baseAreaCells={[[1]]}/>));
     
@@ -40,4 +41,16 @@ describe("Base Area wrapper with same Arrange phase", () => {
         baseAreaWrapper.find('#gameDefaultsId').simulate('click');
         expect(baseAreaWrapper.instance().state.filledBoxes.length).toBe(0);
     })
+});
+
+describe('Base Area wrapper to check state for game status i.e. finished/tied etc.', () => {
+    let baseAreaWrapper;
+    beforeEach(() => baseAreaWrapper = shallow(<BaseArea baseAreaCells={Constants.baseAreaCells} winningPatterns={Constants.winningPatterns}/>));
+
+    it('When isGameFinished attribute is set to true, the body should contain a "Game Over!" message', () => {
+        baseAreaWrapper.instance().setState({
+            isGameFinished: true
+        });
+        expect(baseAreaWrapper.text()).toMatch(/Game Over/);
+    });
 });

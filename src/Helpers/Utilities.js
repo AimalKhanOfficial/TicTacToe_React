f53a08d828f1fef1805f8d352a029c5a199338fe
Aimal Khan <aimalkhanofficial@gmail.com>
2019-09-29 13:53:47 -0400

    Check for undefined params. All tests passing!

--- src/Helpers/Utilities.js
+++ src/Helpers/Utilities.js
@@ -1,4 +1,7 @@
 export default function checkResult(allMovesParam, playerParam, winningPatterns){
+    if(!allMovesParam || !winningPatterns){
+        return false;
+    }
     let playerMoves = []
     let allMoves = [...allMovesParam];
     allMoves.reverse().filter(singleMove => singleMove.playerFlag === playerParam).map(singleMove => playerMoves.push(singleMove.cellId)); 

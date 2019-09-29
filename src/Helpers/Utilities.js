517c87475b72e808543aacf01c6a7220e1cbd0b4
Aimal Khan <aimalkhanofficial@gmail.com>
2019-09-29 13:45:56 -0400

    Test for providing valid data to the checkResult method now passing! (Other 3 tests failing because of a missing check - coming in next!)

--- src/Helpers/Utilities.js
+++ src/Helpers/Utilities.js
@@ -1,3 +1,22 @@
 export default function checkResult(allMovesParam, playerParam, winningPatterns){
-    return false;
+    let playerMoves = []
+    let allMoves = [...allMovesParam];
+    allMoves.reverse().filter(singleMove => singleMove.playerFlag === playerParam).map(singleMove => playerMoves.push(singleMove.cellId)); 
+    playerMoves = playerMoves.sort((a, b) => a - b);
+    let resultCalculated = false;
+    let patternItemExist = true;
+    winningPatterns.forEach(winningPattern => {
+        winningPattern = winningPattern.sort((a, b) => a - b);
+        patternItemExist = true;
+        winningPattern.forEach(patternItem => {
+            if (!playerMoves.toString().includes(patternItem)) {
+                patternItemExist = false;
+                return;
+            }
+        });
+        if(patternItemExist){
+            resultCalculated = true;
+        }
+    });
+    return resultCalculated ? true : false
 }

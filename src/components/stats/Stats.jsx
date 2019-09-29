6bbdd72548aa18621ce2c87c7efe8d0d3eb123d8
Aimal Khan <aimalkhanofficial@gmail.com>
2019-09-29 15:52:20 -0400

    Adding Cosmetic changes for Stats component as part of the refactor process.

--- src/components/stats/Stats.jsx
+++ src/components/stats/Stats.jsx
@@ -4,15 +4,18 @@ export default class Stats extends Component {
 
     render(){
       return (
-        <div>
+        <div className="stats_body">
             <div>
-              <p>Player One: <strong>{this.props.playerOneWins}</strong></p>
+                <img className='avatar' src='/imgs/user1.png' alt='player img' /> 
+                <p>Player One: <strong>{this.props.playerOneWins}</strong></p>
             </div>
             <div>
-              <p>Ties: <strong>{this.props.ties}</strong></p>
+                <img className='avatar' src="/imgs/controller.png" alt='avatar'/> 
+                <p>Ties: <strong>{this.props.ties}</strong></p>
             </div>
             <div>
-              <p>Player Two: <strong>{this.props.playerTwoWins}</strong></p>
+                <img className='avatar' src='/imgs/user1.png' alt='player img' /> 
+                <p>Player Two: <strong>{this.props.playerTwoWins}</strong></p>
             </div>
         </div>
       );

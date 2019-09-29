52343516c2c652c8e07085c2b7c8017a670645bc
Aimal Khan <aimalkhanofficial@gmail.com>
2019-09-29 15:59:34 -0400

    All tests passing for Stats component!

--- src/components/stats/Stats.jsx
+++ src/components/stats/Stats.jsx
@@ -1,4 +1,5 @@
 import React, {Component} from "react";
+import './Stats.css';
 
 export default class Stats extends Component {
 
@@ -7,15 +8,15 @@ export default class Stats extends Component {
         <div className="stats_body">
             <div>
                 <img className='avatar' src='/imgs/user1.png' alt='player img' /> 
-                <p>Player One: <strong>{this.props.playerOneWins}</strong></p>
+                <p>Player One: <strong>{!this.props.playerOneWins ? 'X' : this.props.playerOneWins}</strong></p>
             </div>
             <div>
                 <img className='avatar' src="/imgs/controller.png" alt='avatar'/> 
-                <p>Ties: <strong>{this.props.ties}</strong></p>
+                <p>Ties: <strong>{!this.props.ties ? 'X' : this.props.ties}</strong></p>
             </div>
             <div>
                 <img className='avatar' src='/imgs/user1.png' alt='player img' /> 
-                <p>Player Two: <strong>{this.props.playerTwoWins}</strong></p>
+                <p>Player Two: <strong>{!this.props.playerTwoWins ? 'X' : this.props.playerTwoWins}</strong></p>
             </div>
         </div>
       );
